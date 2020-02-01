2ce43db88144ecaf48bafd766e021fde2a127caf
Robbyzhg <zhangrobby@yandex.com>
2020-02-01 20:22:48 +0700

    3

---
+++ application/controllers/Cart.php
@@ -0,0 +1,8 @@
+<?php 
+
+class cart extends CI_Controller {
+	public function index()
+	{
+		$this->load->view('home/cart');
+	}
+}
--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -1,5 +1,8 @@
 <?php 
 
 class Home extends CI_Controller {
-	
+	public function index()
+	{
+		$this->load->view('home/index');
+	}
 }
