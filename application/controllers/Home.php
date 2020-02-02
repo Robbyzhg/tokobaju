3e021a374c6c0a53d4cd28ee378af89ea4d56526
Robbyzhg <zhangrobby@yandex.com>
2020-02-02 11:35:23 +0700

    4

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -3,6 +3,8 @@
 class Home extends CI_Controller {
 	public function index()
 	{
+		$this->load->view('home/templates/header');
 		$this->load->view('home/index');
+		$this->load->view('home/templates/footer');
 	}
 }
